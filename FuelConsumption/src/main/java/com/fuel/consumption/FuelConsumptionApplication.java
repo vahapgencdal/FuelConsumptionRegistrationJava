72a4dd9166d2d2ab7e03c6eef68b193d26c2731c
vahapgencdal <avahap19@gmail.com>
2020-10-14 22:07:16 +0300

    - The Project created on spring.starter.io and imported to under github path. - for production and test environment ready: application-test.properties and application-prod.properties files added additionally application-dev.properties added for developers. - if you want to change environment goto application.properties and try to change spring.profiles.active to dev,test,prod -logback configuration file added for log configuration for production ready logback prod configuration added
    --next step will add spring rest documentation, swagger will be added to system and
    for testing one api will be added to system

---
+++ FuelConsumption/src/main/java/com/fuel/consumption/FuelConsumptionApplication.java
@@ -0,0 +1,13 @@
+package com.fuel.consumption;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FuelConsumptionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FuelConsumptionApplication.class, args);
+	}
+
+}
