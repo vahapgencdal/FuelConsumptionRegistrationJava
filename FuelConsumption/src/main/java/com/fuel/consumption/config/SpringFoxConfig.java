90c011a85eeba18bf13a35a80ffb6919d15974cc
vahapgencdal <avahap19@gmail.com>
2020-10-16 10:43:28 +0300

    - RestConfig reformatted - SpringFoxConfig reformatted and api path config removed - logback-spring.xml unnecessary includes removed - logback-test.xml config added for unit tests - application.properties added for test configurations

--- FuelConsumption/src/main/java/com/fuel/consumption/config/SpringFoxConfig.java
+++ FuelConsumption/src/main/java/com/fuel/consumption/config/SpringFoxConfig.java
@@ -2,11 +2,8 @@ package com.fuel.consumption.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.bind.annotation.RequestMethod;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.builders.ResponseMessageBuilder;
-import springfox.documentation.schema.ModelRef;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
@@ -14,7 +11,6 @@ import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -31,14 +27,14 @@ public class SpringFoxConfig {
             "Apache 2.0", "http://www.apache.org/licenses/LICENSE-2.0",Arrays.asList());
 
     private static final Set<String> DEFAULT_PRODUCES_AND_CONSUMES =
-            new HashSet<String>(Arrays.asList("application/json","application/xml"));
+            new HashSet<>(Arrays.asList("application/json"));
 
 
     @Bean
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2).select()
-                .apis(RequestHandlerSelectors.basePackage("com.fuel.consumption.api"))
-                .paths(PathSelectors.ant("/api/v1/*"))
+                .apis(RequestHandlerSelectors.basePackage("com.fuel.consumption.api.v1"))
+                .paths(PathSelectors.any())
                 .build()
                 .apiInfo(DEFAULT_API_INFO)
                 .produces(DEFAULT_PRODUCES_AND_CONSUMES)
--- FuelConsumption/src/test/java/com/fuel/consumption/FuelConsumptionApplicationTests.java
+++ FuelConsumption/src/test/java/com/fuel/consumption/FuelConsumptionApplicationTests.java
@@ -1,13 +1,18 @@
 package com.fuel.consumption;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("logging-test")
+@Slf4j
 class FuelConsumptionApplicationTests {
 
 	@Test
 	void contextLoads() {
+		log.error("asdasdasdasd");
 	}
 
 }
