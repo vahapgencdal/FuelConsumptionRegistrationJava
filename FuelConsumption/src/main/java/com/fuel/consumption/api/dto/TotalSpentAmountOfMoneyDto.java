f961723b727e9ab3f770a21a8aef6b628ff61be3
vahapgencdal <avahap19@gmail.com>
2020-10-22 17:07:55 +0300

    - sonar error handled

--- FuelConsumption/src/main/java/com/fuel/consumption/api/dto/TotalSpentAmountOfMoneyDto.java
+++ FuelConsumption/src/main/java/com/fuel/consumption/api/dto/TotalSpentAmountOfMoneyDto.java
@@ -2,7 +2,6 @@ package com.fuel.consumption.api.dto;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fuel.consumption.util.BigDecimalUtil;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
